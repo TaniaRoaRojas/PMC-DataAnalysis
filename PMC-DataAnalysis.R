c9a37bd22ee1777115e0ef3fec55a8cacebedcd7
TaniaRoeRojas <taniaroarojas@gmail.com>
2022-10-05 22:41:58 -0300

    separación por ocupación y género

--- PMC-DataAnalysis.R
+++ PMC-DataAnalysis.R
@@ -1,11 +1,24 @@
 ## read data set 
-data = read.table(file.choose(), header = T, sep = ';')
+data = read.table(file.choose(), header = T, sep = ';') #buscar 'consolidado'
 View(data)
 
+## D3 column refers to the occupation
+data$D3
 
-##i'm going to eliminate the first row
-data = data[-1,]
-View(data)
+##lawyer's data
+data_law = data[data$D3 =='Derecho',]
+View(data_law)
+
+
+##doctor's data
+data_doc = data[data$D3 =='Medicina',]
+View(data_doc)
+
+
+## D2 column refers to gender
+data_fem = data[data$D2 =='Femenino',]
+data_male = data[data$D2 =='Masculino',]
+
 
 ## Create box plot per group (doctors and lawers)
 
